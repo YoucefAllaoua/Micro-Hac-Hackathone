f4fab83db11aa333456e9bc1a0edaedf866d4524
Youcef_Allaoua <youcefallaoua04@gmail.com>
2023-03-03 22:57:31 +0100

    youcef

--- public/js/market.js
+++ public/js/market.js
@@ -2,6 +2,24 @@ const link = "http://localhost:5000/api/v1/marketplace-data";
 
 method = { method: "GET" };
 
-fetch(link, method).then((data) => {
-	console.log(data);
-});
+const getData = async () => {
+	const data_container = document.querySelector(".data .container");
+	let { data } = await axios.get(link);
+	data = data.data;
+	data = data.map((datum) => {
+		const { owner, title, desc, img, category } = datum;
+		console.log(datum);
+		return `<div class="datum">
+					<div class="image">
+						<img src=${img} alt="" />
+					</div>
+					<div class="text">
+						<p class="title">${title}</p>
+						<p class="prov">Data provider : <span class="provider">${owner}</span></p>
+						<p class="desc">${desc}</p>
+					</div>
+				</div>`;
+	});
+	data_container.innerHTML = data.join("");
+};
+getData();
